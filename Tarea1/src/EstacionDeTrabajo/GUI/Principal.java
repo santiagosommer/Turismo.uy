b166f3c1d8cebc78bc9283cf428528abe273edd3
Nicolas Pereira <nicolas.pereira.peraza@fing.edu.uy>
2022-08-23 23:47:11 -0300

    Cambio GUI Principal

--- Tarea1/src/EstacionDeTrabajo/GUI/Principal.java
+++ Tarea1/src/EstacionDeTrabajo/GUI/Principal.java
@@ -6,12 +6,18 @@ import ServidorCentral.Logica.Interfaces.*;
 import ServidorCentral.Logica.Fabrica.Fabrica;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JInternalFrame;
+import java.awt.BorderLayout;
 
 public class Principal {
 
 	
 	
-	private JFrame frame;
+	private JFrame frmServidorcentral;
+	private AltaDeUsuario altaUsuarioInternalFrame;
     private IUsuario ICUsu;
     private ITuristica ICTuri;
     private IPaquete ICP;
@@ -35,7 +41,7 @@ public class Principal {
 			public void run() {
 				try {
 					Principal window = new Principal();
-					window.frame.setVisible(true);
+					window.frmServidorcentral.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -67,9 +73,32 @@ public class Principal {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmServidorcentral = new JFrame();
+		frmServidorcentral.setTitle("Servidor Central");
+		frmServidorcentral.setBounds(100, 100, 450, 300);
+		frmServidorcentral.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frmServidorcentral.setJMenuBar(menuBar);
+		
+		JMenu menuSistema = new JMenu("Sistema");
+		menuBar.add(menuSistema);
+		
+		JMenuItem menuItemSalir = new JMenuItem("Salir");
+		menuSistema.add(menuItemSalir);
+		
+		JMenu menuUsuario = new JMenu("Usuario");
+		menuBar.add(menuUsuario);
+		
+		JMenuItem menuItemAltaDeUsuario = new JMenuItem("Alta de Ususario");
+		menuItemAltaDeUsuario.setSelected(true);
+		menuUsuario.add(menuItemAltaDeUsuario);
+		
+		altaUsuarioInternalFrame = new AltaDeUsuario(ICUsu);
+		altaUsuarioInternalFrame.setBounds(28, 37, 370, 123);
+		altaUsuarioInternalFrame.setVisible(false);
+		frmServidorcentral.getContentPane().setLayout(null);
+		frmServidorcentral.getContentPane().add(altaUsuarioInternalFrame);
 	}
 
 }
