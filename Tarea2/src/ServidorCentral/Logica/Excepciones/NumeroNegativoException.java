7da79fc33ebc8c2383d0f2372659c9cbeceefd07
sommer <santisommer52@gmail.com>
2022-09-07 16:16:55 -0300

    numero neg

---
+++ Tarea2/src/ServidorCentral/Logica/Excepciones/NumeroNegativoException.java
@@ -0,0 +1,10 @@
+package ServidorCentral.Logica.Excepciones;
+
+@SuppressWarnings("serial")
+public class NumeroNegativoException extends Exception {
+	public NumeroNegativoException(String string) {
+		super(string);
+}
+	
+
+}
