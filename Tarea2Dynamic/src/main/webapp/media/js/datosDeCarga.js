2548e6f5e1aff801e1963ffdf770a0c9a60eb7ef
Nicolas Pereira <nicolas.pereira.peraza@fing.edu.uy>
2022-10-10 12:36:23 -0300

    Proyecto Dynamic

---
+++ Tarea2Dynamic/src/main/webapp/media/js/datosDeCarga.js
@@ -0,0 +1,18 @@
+export let baseDeUsuarios = [
+    {
+        "nickname": "lachiqui",
+        "nombre": "Rosa Maria",
+        "apellido": "Martınez",
+        "email": "mirtha.legrand.ok@hotmail.com.ar",
+        "password": "rosita"
+    },
+    {
+        "nickname": "washington",
+        "nombre": "Washington",
+        "apellido": "Rocha",
+        "email": "washington@turismorocha.gub.uy",
+        "password": "washi123"
+    }
+];
+
+//No se puede usar a no ser que se haga un server local.
