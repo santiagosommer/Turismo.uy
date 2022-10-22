224d66417b99acf17d152bcbfe26b3ce412abd01
Nicolas Pereira <nicolas.pereira.peraza@fing.edu.uy>
2022-10-22 15:32:55 -0300

    Jar Exportado de la logica de tarea 2

--- Tarea2Dynamic/src/main/java/ServidorCentral/Logica/Excepciones/UsuarioNoExisteException.java
+++
@@ -1,9 +0,0 @@
-package ServidorCentral.Logica.Excepciones;
-
-@SuppressWarnings("serial")
-public class UsuarioNoExisteException extends Exception {
-
-    public UsuarioNoExisteException(String string) {
-        super(string);
-    }
-}
