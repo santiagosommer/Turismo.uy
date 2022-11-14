c91ad4cea70e79d53ce114a86512acd7b94ea3d0
vagrant <vagrant@kubuntu>
2022-11-14 20:22:13 -0300

    NICOLAAAAAAAAAAAAS

---
+++ ServidorWS/src/WebService/webservice/SetDTUsuario.java
@@ -0,0 +1,32 @@
+
+package webservice;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para setDTUsuario complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>{@code
+ * <complexType name="setDTUsuario">
+ *   <complexContent>
+ *     <restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       <sequence>
+ *       </sequence>
+ *     </restriction>
+ *   </complexContent>
+ * </complexType>
+ * }</pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "setDTUsuario")
+public class SetDTUsuario {
+
+
+}
