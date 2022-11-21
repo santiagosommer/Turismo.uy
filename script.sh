fbb90eb996e159d7835480d898cf7fb9a4816853
Santiago Sommer Glasner - ENS <santiago.sommer@pcunix153.fing.edu.uy>
2022-11-21 15:28:12 -0300

    serverws pom y script

---
+++ script.sh
@@ -0,0 +1,8 @@
+PROJECT_ABSOLUT_PATH=/ens/devel01/tpgr25/tpgr252/
+cd $PROJECT_ABSOLUT_PATH
+cd ServidorWS
+mvn clean install
+cd ../Tarea2Test
+mvn clean install
+cd ../TurismoMobile
+mvn clean install
