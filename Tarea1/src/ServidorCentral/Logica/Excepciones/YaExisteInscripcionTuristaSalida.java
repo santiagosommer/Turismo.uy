01bfaaea675c9db10a00aac2987810d9a5b69a9d
Santiago Sommer <103203556+santiagosommer@users.noreply.github.com>
2022-11-24 00:38:37 -0300

    Delete Tarea1 directory

--- Tarea1/src/ServidorCentral/Logica/Excepciones/YaExisteInscripcionTuristaSalida.java
+++
@@ -1,9 +0,0 @@
-package ServidorCentral.Logica.Excepciones;
-
-@SuppressWarnings("serial")
-public class YaExisteInscripcionTuristaSalida extends Exception {
-	public YaExisteInscripcionTuristaSalida(String string) {
-		super(string);
-    }
-
-}
